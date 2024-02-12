3023de0a2141a00f1f850bb9c17dd18811e209c9
7_DK <150216260+ssunxiangrui@users.noreply.github.com>
2024-02-12 10:14:56 +0800

    Update Links.vue

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -44,7 +44,6 @@
 import { Icon } from "@vicons/utils";
 // 可前往 https://www.xicons.org 自行挑选并在此处引入
 import { Link, Blog, CompactDisc, Cloud, Compass, Book, Fire, LaptopCode, Forumbee, Image} from "@vicons/fa"; // 注意使用正确的类别
-import { Forum } from "@vicons/carbon";
 import { mainStore } from "@/store";
 import { Swiper, SwiperSlide } from "swiper/vue";
 import { Pagination, Mousewheel } from "swiper";
